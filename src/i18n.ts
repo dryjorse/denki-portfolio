4a5bd1ab5d2565faa2ba616067902f2963d35e0a
dryjorse <kozoevabdulazim@gmail.com>
2024-06-30 03:22:29 +0600

    translater

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,28 @@
 import i18n from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
-  .init({ fallbackLng: "en" });
+  .init({
+    // @ts-ignore
+    lng: localStorage.getItem("selectedLanguage"),
+    fallbackLng: "en",
+
+    debug: false,
+    detection: {
+      order: ["queryString", "cookie"],
+      cache: ["cookie"],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: "/denki-portfolio-site/locales/{{lng}}/translation.json",
+    },
+  });
 
 export default i18n;
