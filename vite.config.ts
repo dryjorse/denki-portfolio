6517e0b8daa84da203f0ad20d7b6e7a50bb3e77a
dryjorse <kozoevabdulazim@gmail.com>
2024-06-30 15:43:19 +0600

    git fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/denki-portfolio-site",
+  base: "/denki-portfolio",
   plugins: [react()],
   server: {
     host: "localhost",
